7d23e664a29d99816b6cf268ed4c6a1efb6e8bbd
TonyynoT <tonynguyen0403@gmail.com>
2021-04-09 23:22:36 +0930

    added couple routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,28 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/login', function(req, res, next) {
+  //es.render('index', { title: 'Express' });
+});
+
+
+router.get('/signup', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+});
+
+
+router.get('/profile', function(req, res, next) {
+   //res.render('index', { title: 'Express' });
+});
+
+
+router.get('/reserve', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+});
+
+
+
+
+
 module.exports = router;
